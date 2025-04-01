a612f0b0a6065f4645e1195faa79d6971fbca83c
Kevin <77134229+KevinSjr@users.noreply.github.com>
2025-04-01 20:40:06 -0300

    Update Mavenproject4.java

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
@@ -11,8 +11,12 @@ package com.mycompany.mavenproject4;
 public class Mavenproject4 {
 
     public static void main(String[] args) {
+        Circulo c1 = new Circulo ("circulo",4.0);
        Quadrado q1 = new Quadrado("nome",2.0,2.0,2.0);
+         Retangulo r1 = new Retangulo ("circulo",2.0,2.0);
        q1.CalcularArea();
+       c1.CalcularArea();
+       r1.CalcularArea();
     }
     
 }
