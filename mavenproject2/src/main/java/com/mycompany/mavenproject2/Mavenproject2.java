6b2eb84894a44e659fb4ed9ff8085fc2edf38183
Kevin <77134229+KevinSjr@users.noreply.github.com>
2025-04-01 20:35:10 -0300

    Update Mavenproject2.java

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -12,8 +12,10 @@ public class Mavenproject2 {
 
     public static void main(String[] args) {
     Carro carro1 = new Carro ("modelo","placa",1,3);
-    
+    Moto moto1 = new Moto ("modelo2","placa2",2,2);
+    Veiculo v1 = new Veiculo("modelo3","placa3",3);
     carro1.exibirInfo();
-    
+    moto1.exibirInfo();
+    v1.exibirInfo();
     }
 }
